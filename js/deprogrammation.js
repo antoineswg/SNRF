6c7b9d73ea086a6491cf199dee52cc18aefefabc
antoine <153529541+antoineswg@users.noreply.github.com>
2024-11-26 14:31:05 +0100

    réorganisation et séparation des fichiers css

--- js/deprogrammation.js
+++ js/deprogrammation.js
@@ -6,25 +6,25 @@ function updateDisplay(data) {
 
     document.querySelector('.deprog2020 .anneeDeprog').innerHTML = ligne[0].year;
     document.querySelector('.deprog2020 .pourcentageDeprog').innerHTML = ligne[0].percentage + '%';
-    document.querySelector('.deprog2020 .logoDeprog').innerHTML = '<img src="images/logosLignes/' + ligneActive + 'Logo.png" alt=""></img>';
+    document.querySelector('.deprog2020 .logoDeprog').innerHTML = '<img src="styles/images/logosLignes/' + ligneActive + 'Logo.png" alt=""></img>';
 
     document.querySelector('.deprog2021 .anneeDeprog').innerHTML = ligne[1].year;
     document.querySelector('.deprog2021 .pourcentageDeprog').innerHTML = ligne[1].percentage + '%';
-    document.querySelector('.deprog2021 .logoDeprog').innerHTML = '<img src="images/logosLignes/' + ligneActive + 'Logo.png" alt=""></img>';
+    document.querySelector('.deprog2021 .logoDeprog').innerHTML = '<img src="styles/images/logosLignes/' + ligneActive + 'Logo.png" alt=""></img>';
 
     document.querySelector('.deprog2022 .anneeDeprog').innerHTML = ligne[2].year;
     document.querySelector('.deprog2022 .pourcentageDeprog').innerHTML = ligne[2].percentage + '%';
-    document.querySelector('.deprog2022 .logoDeprog').innerHTML = '<img src="images/logosLignes/' + ligneActive + 'Logo.png" alt=""></img>';
+    document.querySelector('.deprog2022 .logoDeprog').innerHTML = '<img src="styles/images/logosLignes/' + ligneActive + 'Logo.png" alt=""></img>';
 
     document.querySelector('.deprog2023 .anneeDeprog').innerHTML = ligne[3].year;
     document.querySelector('.deprog2023 .pourcentageDeprog').innerHTML = ligne[3].percentage + '%';
-    document.querySelector('.deprog2023 .logoDeprog').innerHTML = '<img src="images/logosLignes/' + ligneActive + 'Logo.png" alt=""></img>';
+    document.querySelector('.deprog2023 .logoDeprog').innerHTML = '<img src="styles/images/logosLignes/' + ligneActive + 'Logo.png" alt=""></img>';
 
     let percentages = ligne.map(item => parseInt(item.percentage, 10));
     let moyenne = (percentages[0] + percentages[1] + percentages[2] + percentages[3]) / 4;
     document.querySelector('.deprogMoyenne .anneeDeprog').innerHTML = 'Moyenne';
     document.querySelector('.deprogMoyenne .pourcentageDeprog').innerHTML = moyenne.toFixed(1) + '%';
-    document.querySelector('.deprogMoyenne .logoDeprog').innerHTML = '<img src="images/logosLignes/' + ligneActive + 'Logo.png" alt=""></img>';
+    document.querySelector('.deprogMoyenne .logoDeprog').innerHTML = '<img src="styles/images/logosLignes/' + ligneActive + 'Logo.png" alt=""></img>';
 
 }
 
