3ebeb4d36a4bdfa3061a0483cb1c3fc0d8bac0ae
antoine <153529541+antoineswg@users.noreply.github.com>
2024-11-26 14:41:37 +0100

    optimisation de la méthode d'affichage des données

--- js/ponctualite.js
+++ js/ponctualite.js
@@ -3,131 +3,38 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.json())
         .then(data => {
             const labels = data.data.lignea.map(item => item.year);
-            const percentageDataLigneA = data.data.lignea.map(item => parseFloat(item.percentage.replace(',', '.')));
-            const percentageDataLigneB = data.data.ligneb.map(item => parseFloat(item.percentage.replace(',', '.')));
-            const percentageDataLigneC = data.data.lignec.map(item => parseFloat(item.percentage.replace(',', '.')));
-            const percentageDataLigneD = data.data.ligned.map(item => parseFloat(item.percentage.replace(',', '.')));
-            const percentageDataLigneE = data.data.lignee.map(item => parseFloat(item.percentage.replace(',', '.')));
-            const percentageDataLigneH = data.data.ligneh.map(item => parseFloat(item.percentage.replace(',', '.')));
-            const percentageDataLigneJ = data.data.lignej.map(item => parseFloat(item.percentage.replace(',', '.')));
-            const percentageDataLigneK = data.data.lignek.map(item => parseFloat(item.percentage.replace(',', '.')));
-            const percentageDataLigneN = data.data.lignen.map(item => parseFloat(item.percentage.replace(',', '.')));
-            const percentageDataLigneP = data.data.lignep.map(item => parseFloat(item.percentage.replace(',', '.')));
-            const percentageDataLigneR = data.data.ligner.map(item => parseFloat(item.percentage.replace(',', '.')));
-            const percentageDataLigneU = data.data.ligneu.map(item => parseFloat(item.percentage.replace(',', '.')));
-            const percentageDataLigneL = data.data.lignel.map(item => parseFloat(item.percentage.replace(',', '.')));
 
             var ctx = document.getElementById('ponctualiteChart').getContext('2d');
+            const colors = {
+                lignea: { bg: 'rgba(227, 5, 28, 0.3)', border: 'rgba(227, 5, 28, 1)' },
+                ligneb: { bg: 'rgba(82, 145, 206, 0.3)', border: 'rgba(82, 145, 206, 1)' },
+                lignec: { bg: 'rgba(255, 206, 0, 0.3)', border: 'rgba(255, 206, 0, 1)' },
+                ligned: { bg: 'rgba(0, 129, 79, 0.3)', border: 'rgba(0, 129, 79, 1)' },
+                lignee: { bg: 'rgba(192, 65, 145, 0.3)', border: 'rgba(192, 65, 145, 1)' },
+                ligneh: { bg: 'rgba(141, 94, 42, 0.3)', border: 'rgba(141, 94, 42, 1)' },
+                lignej: { bg: 'rgba(213, 201, 0, 0.3)', border: 'rgba(213, 201, 0, 1)' },
+                lignek: { bg: 'rgba(159, 152, 37, 0.3)', border: 'rgba(159, 152, 37, 1)' },
+                lignel: { bg: 'rgba(206, 173, 210, 0.3)', border: 'rgba(206, 173, 210, 1)' },
+                lignen: { bg: 'rgba(0, 168, 143, 0.3)', border: 'rgba(0, 168, 143, 1)' },
+                lignep: { bg: 'rgba(242, 142, 66, 0.3)', border: 'rgba(242, 142, 66, 1)' },
+                ligner: { bg: 'rgba(243, 164, 186, 0.3)', border: 'rgba(243, 164, 186, 1)' },
+                ligneu: { bg: 'rgba(185, 8, 69, 0.3)', border: 'rgba(185, 8, 69, 1)' }
+            };
+
+            const datasets = Object.keys(colors).map((key, index) => ({
+                label: `Ligne ${key.toUpperCase().replace('LIGNE', '')}`,
+                data: data.data[key].map(item => parseFloat(item.percentage.replace(',', '.'))),
+                backgroundColor: colors[key].bg,
+                borderColor: colors[key].border,
+                borderWidth: 3,
+                hidden: index !== 0
+            }));
+
             var myChart = new Chart(ctx, {
                 type: 'line',
                 data: {
                     labels: labels,
-                    datasets: [
-                        {
-                            label: 'RER A',
-                            data: percentageDataLigneA,
-                            backgroundColor: 'rgba(227, 5, 28, 0.3)',
-                            borderColor: 'rgba(227, 5, 28, 1)',
-                            borderWidth: 3,
-                            hidden: false 
-                        },
-                        {
-                            label: 'RER B',
-                            data: percentageDataLigneB,
-                            backgroundColor: 'rgba(82, 145, 206, 0.3)',
-                            borderColor: 'rgba(82, 145, 206, 1)',
-                            borderWidth: 3,
-                            hidden: true 
-                        },
-                        {
-                            label: 'RER C',
-                            data: percentageDataLigneC,
-                            backgroundColor: 'rgba(255, 206, 0, 0.3)',
-                            borderColor: 'rgba(255, 206, 0, 1)',
-                            borderWidth: 3,
-                            hidden: true 
-                        },
-                        {
-                            label: 'RER D',
-                            data: percentageDataLigneD,
-                            backgroundColor: 'rgba(0, 129, 79, 0.3)',
-                            borderColor: 'rgba(0, 129, 79, 1)',
-                            borderWidth: 3,
-                            hidden: true 
-                        },
-                        {
-                            label: 'RER E',
-                            data: percentageDataLigneE,
-                            backgroundColor: 'rgba(192, 65, 145, 0.3)',
-                            borderColor: 'rgba(192, 65, 145, 1)',
-                            borderWidth: 3,
-                            hidden: true 
-                        },
-                        {
-                            label: 'Transilien H',
-                            data: percentageDataLigneH,
-                            backgroundColor: 'rgba(141, 94, 42, 0.3)',
-                            borderColor: 'rgba(141, 94, 42, 1)',
-                            borderWidth: 3,
-                            hidden: true 
-                        },
-                        {
-                            label: 'Transilien J',
-                            data: percentageDataLigneJ,
-                            backgroundColor: 'rgba(213, 201, 0, 0.3)',
-                            borderColor: 'rgba(213, 201, 0, 1)',
-                            borderWidth: 3,
-                            hidden: true 
-                        },
-                        {
-                            label: 'Transilien K',
-                            data: percentageDataLigneK,
-                            backgroundColor: 'rgba(159, 152, 37, 0.3)',
-                            borderColor: 'rgba(159, 152, 37, 1)',
-                            borderWidth: 3,
-                            hidden: true 
-                        },
-                        {
-                            label: 'Transilien L',
-                            data: percentageDataLigneL,
-                            backgroundColor: 'rgba(206, 173, 210, 0.3)',
-                            borderColor: 'rgba(206, 173, 210, 1)',
-                            borderWidth: 3,
-                            hidden: true 
-                        },
-                        {
-                            label: 'Transilien N',
-                            data: percentageDataLigneN,
-                            backgroundColor: 'rgba(0, 168, 143, 0.3)',
-                            borderColor: 'rgba(0, 168, 143, 1)',
-                            borderWidth: 3,
-                            hidden: true 
-                        },
-                        {
-                            label: 'Transilien P',
-                            data: percentageDataLigneP,
-                            backgroundColor: 'rgba(242, 142, 66, 0.3)',
-                            borderColor: 'rgba(242, 142, 66, 1)',
-                            borderWidth: 3,
-                            hidden: true 
-                        },
-                        {
-                            label: 'Transilien R',
-                            data: percentageDataLigneR,
-                            backgroundColor: 'rgba(243, 164, 186, 0.3)',
-                            borderColor: 'rgba(243, 164, 186, 1)',
-                            borderWidth: 3,
-                            hidden: true 
-                        },
-                        {
-                            label: 'Transilien U',
-                            data: percentageDataLigneU,
-                            backgroundColor: 'rgba(185, 8, 69, 0.3)',
-                            borderColor: 'rgba(185, 8, 69, 1)',
-                            borderWidth: 3,
-                            hidden: true 
-                        }
-                    ]
+                    datasets: datasets
                 },
                 options: {
                     scales: {
